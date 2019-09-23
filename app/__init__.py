0d6bc33af114f6414dd2773dd143cffd534c4c09
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-09-23 16:45:15 +0200

    Undo setting of pool_pre_ping as this issue was most likely caused by something else

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,10 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from config import Config
 from flask import Flask
 from flask_babel import Babel
-from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-
-
-class SQLAlchemy(_BaseSQLAlchemy):
-    def apply_pool_defaults(self, app, options):
-        super(SQLAlchemy, self).apply_pool_defaults(app, options)
-        options["pool_pre_ping"] = True
 
 
 app = Flask(__name__)
