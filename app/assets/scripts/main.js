569bfc6ac7242f5e4d63be51eb5762b48b5eb197
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-09-27 13:20:21 +0200

    Set Bootstrap-Table local to Dutch; style payment table search

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -2,6 +2,7 @@
 import 'jquery';
 import 'bootstrap';
 import 'bootstrap-table';
+import 'bootstrap-table/dist/locale/bootstrap-table-nl-NL.min.js';
 import 'bootstrap-table/dist/extensions/sticky-header/bootstrap-table-sticky-header.min.js';
 import 'bootstrap-table/dist/extensions/mobile/bootstrap-table-mobile.min.js';
 import naturalSort from 'javascript-natural-sort';
