f79f6ba3ea35612b40292e3677fa9622a91050b4
Sicco van Sas <siccovansas@users.noreply.github.com>
2019-09-30 12:31:13 +0200

    Fix round border styling of table rows which was broken after making the table responsive

--- app/assets/scripts/routes/common.js
+++ app/assets/scripts/routes/common.js
@@ -9,6 +9,15 @@ export default {
             classes: 'table'
         }
     );
+
+    // We need JavaScript to set the rounded border of the last visible element in a tr
+    $('.payment-table tr').find('td:not(.d-none):last').css(
+      {
+        'border-right-style': 'solid',
+        'border-bottom-right-radius': '35px',
+        'border-top-right-radius': '35px'
+      }
+    )
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
