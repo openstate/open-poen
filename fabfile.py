4d889c5c3f98f63c233bc11b12c9213105f2c787
Sicco van Sas <siccovansas@users.noreply.github.com>
2020-05-25 18:02:00 +0200

    Update fabfile to also deploy Corona version of Open Poen

--- fabfile.py
+++ fabfile.py
@@ -7,12 +7,15 @@ GIT_REPO = 'open-poen'
 
 # Path of the directory
 DIR = '/home/projects/%s' % (GIT_REPO)
+DIR_CORONA = '/home/projects/%s-corona' % (GIT_REPO)
 
 # Container used to compile the assets
 NODE_CONTAINER = 'poen_node_1'
+NODE_CONTAINER_CORONA = 'openpoencorona_node_1'
 
 # App container
 APP_CONTAINER = 'poen_app_1'
+APP_CONTAINER_CORONA = 'openpoencorona_app_1'
 
 # Server name
 SERVER = 'Oxygen'
@@ -51,3 +54,43 @@ def deploy(c):
 
     # Reload app
     c.run('bash -c "cd %s && touch uwsgi-touch-reload"' % (DIR))
+
+
+@task
+def deploy_corona(c):
+    sudo_pass = getpass.getpass("Enter your sudo password on %s: " % SERVER)
+    config = Config(overrides={'sudo': {'password': sudo_pass}})
+    c = Connection(SERVER, config=config)
+
+    # Pull from GitHub
+    print('bash -c "cd %s && git pull git@github.com:openstate/%s.git"' % (
+            DIR_CORONA,
+            GIT_REPO
+        )
+    )
+    c.run(
+        'bash -c "cd %s && git pull git@github.com:openstate/%s.git"' % (
+            DIR_CORONA,
+            GIT_REPO
+        )
+    )
+
+    # Compile assets
+    output = c.sudo(
+        'docker inspect --format="{{.State.Status}}" %s' % (NODE_CONTAINER_CORONA)
+    )
+    if output.stdout.strip() != 'running':
+        raise Exit(
+            '\n*** ERROR: The %s container, used to compile the assets, is '
+            'not running. Please build/run/start the container.' % (
+                NODE_CONTAINER_CORONA
+            )
+        )
+    c.sudo('docker exec %s yarn' % (NODE_CONTAINER_CORONA))
+    c.sudo('docker exec %s yarn prod' % (NODE_CONTAINER_CORONA))
+
+    # Upgrade database
+    #c.sudo('docker exec %s flask db upgrade' % (APP_CONTAINER))
+
+    # Reload app
+    c.run('bash -c "cd %s && touch uwsgi-touch-reload"' % (DIR_CORONA))
