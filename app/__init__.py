479d16836f15efe3829ce1cd34e9cb3c98063784
Sicco van Sas <siccovansas@users.noreply.github.com>
2020-07-07 19:45:58 +0200

    Add "Corona" to email subject and startup log message

--- app/__init__.py
+++ app/__init__.py
@@ -54,7 +54,7 @@ if not app.debug:
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr=app.config['FROM'],
             toaddrs=app.config['ADMINS'],
-            subject='[Open Poen] website error',
+            subject='[Open Poen Corona] website error',
             credentials=auth,
             secure=secure
         )
@@ -85,4 +85,4 @@ if app.debug:
     app.logger.setLevel(logging.DEBUG)
 else:
     app.logger.setLevel(logging.INFO)
-app.logger.info('Open Poen startup')
+app.logger.info('Open Poen Corona startup')
